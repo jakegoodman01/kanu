a366a9f83c28176addcf00f9302f8b3dbe954d43
Jake Goodman <jakewgoodman@gmail.com>
2018-06-24 12:48:22 -0400

    Wrote first draft of ExpressionParser, many bugs it is barely working :)

--- expression.py
+++ expression.py
@@ -4,11 +4,22 @@ operations = {'+': Element.add, '-': Element.sub, '*': Element.mul, '/': Element
 
 
 class OperatorList:
-    def __init__(self, operation, *elements):
+    def __init__(self, *elements, operation='+'):
         """ Elements is composed of Elements and OperatorLists
             op is an operation character: +, -, *, /
         """
-        self.members = list(elements)
+        self.members = []
+        for e in elements:
+            if isinstance(e, Element):
+                self.members.append(e)
+            elif isinstance(e, OperatorList):
+                if len(e.members) > 1 and operation in ('*', '/'):
+                    self.members.append(e)
+                else:
+                    self.members.extend(e.members)
+            else:
+                raise ValueError('Invalid argument for an OperatorList')
+
         self.operation = operation
 
         self.simplify()
@@ -33,14 +44,45 @@ class OperatorList:
                     restart = True
                 except ValueError:
                     j += 1
+                except AttributeError:
+                    j += 1
             if restart:
                 i = 0
             else:
                 i += 1
 
 
+class ExpressionParser:
+    def __init__(self, exp: str):
+        self.exp = ExpressionParser._format_parens(exp)
+        self.elements = ExpressionParser._parse_expression(self.exp)
+
+    @classmethod
+    def _format_parens(cls, exp: str) -> str:
+        exp_list = list(exp)
+        i = 0
+        while i < len(exp_list):
+            if exp_list[i] == '(':
+                exp_list.insert(i, '*')
+                i += 1
+            i += 1
+        return ''.join(exp_list)
+
+    @classmethod
+    def _parse_expression(cls, exp: str) -> OperatorList:
+        exp = ''.join(exp.split())  # remove spaces from exp
+        for i in range(len(exp)):
+            if exp[i] in ('+', '-', '*', '/', '^'):
+                return OperatorList(
+                    OperatorList(ExpressionParser._parse_expression(exp[:i])),
+                    OperatorList(ExpressionParser._parse_expression(exp[i + 1:])),
+                    operation=exp[i]
+                )
+
+        # there are no operators once program flow reaches this point
+        return Element(exp)
+
+
 if __name__ == '__main__':
-    e1 = Element('5a')
-    e2 = Element('6a')
-    e3 = Element('1')
-    print(OperatorList('+', e1, e2, e3))
+    ex = ExpressionParser('a + 2 * b + 1')
+    print(ex.elements)
