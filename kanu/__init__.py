335a43058c2f660e8a436c30fb3ada6dd4b880ea
Jake Goodman <jakewgoodman@gmail.com>
2018-12-19 11:53:08 -0500

    added version variable

--- kanu/__init__.py
+++ kanu/__init__.py
@@ -1,3 +1,5 @@
 from .element import get_matching_paren, InvalidVariableError, Element, Variable, InvalidElementError
 from .expression import OperatorList, format_parens, parse_expression, to_op_list, to_rpn, all_together_now
 from .equation import find_variables, solve_single_linear_equation, NonLinearEquationError
+
+__version__ = '0.1.1'
