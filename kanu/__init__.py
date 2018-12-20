bd8028d8b227391866eed0100b2de5e94e3f8164
Jake Goodman <jakewgoodman@gmail.com>
2018-12-20 09:33:26 -0500

    Updated version

--- kanu/__init__.py
+++ kanu/__init__.py
@@ -2,4 +2,4 @@ from .element import get_matching_paren, InvalidVariableError, Element, Variable
 from .expression import OperatorList, format_parens, parse_expression, to_op_list, to_rpn, all_together_now
 from .equation import find_variables, solve_single_linear_equation, NonLinearEquationError
 
-__version__ = '0.1.1'
+__version__ = '0.1.3'
