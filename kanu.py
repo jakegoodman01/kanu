68eeb9d3a6bcc113f9d50ec90d1f17e50e4d5121
Jake Goodman <jakewgoodman@gmail.com>
2018-12-22 15:10:54 -0400

    Added exit option in the menu

--- kanu.py
+++ kanu.py
@@ -5,6 +5,7 @@ while True:
     print('Select one:')
     print('\t1 -> Solve a linear equation')
     print('\t2 -> Simplify any expression')
+    print('\t3 -> Exit program')
 
     choice = input()
     if choice == '1':
@@ -17,6 +18,9 @@ while True:
     elif choice == '2':
         print('Enter the expression:', end=' ')
         print(kanu.all_together_now(input()))
+    elif choice == '3':
+        print('Thank you for using kanu!\nPlease contact me at jake@jakelgoodman.com with any bugs or suggestions.')
+        break
     else:
         print('You must select an option from the menu!')
     print()
